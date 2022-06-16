7a8ab9f0560f6c581aa8b5e5a72ab5a6a541c5d4
kaviarasukumarasamy <107115293+kaviarasukumarasamy@users.noreply.github.com>
2022-06-16 01:43:32 -0700

    error msg

---
+++ Student/src/Dbconnection/Studentdatabase.java
@@ -0,0 +1,32 @@
+package Dbconnection;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+
+public class Studentdatabase {
+	public static void main(String args[]) {
+		String str = "jdbc:postgresql://localhost:5432/Demo_db";
+		String username = "postgres";
+		String pass = "password";
+		try {
+			Connection con = DriverManager.getConnection(str, username, pass);
+			System.out.println("Connected to PostgreSQL Server");
+			String sql = "insert into student (stud_id,stud_name,department,phonenumber)"
+					+ "values ('10','Rajesh','EEE','9876765456')";
+			Statement statement = con.createStatement();
+			int rows = statement.executeUpdate(sql);
+			if (rows > 0) {
+				System.out.println("A new student details has been inserted ");
+			}
+			con.close();
+		} catch (SQLException e) {
+			System.out.println("Error in connecting database");
+			e.printStackTrace();
+		}
+
+	}
+
+}
